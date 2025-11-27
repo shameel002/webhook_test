c964c653628ba7ccd686cd5570b5f826f1ea040b
shameel002 <shameel0505@gmail.com>
2025-11-27 13:03:11 +0400

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Request, HTTPException
 import json
+import os
 from datetime import datetime
+from ai_responder import generate_ticket_reply
 
 app = FastAPI()
 
-WEBHOOK_SECRET = "123456secret"  # must match Freshservice header
-
+WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET") # check the node in the fresh service workflow (remember that this is the security we are using now)
 def verify_secret(request: Request):
     received_secret = request.headers.get("X-Webhook-Secret")
     if received_secret != WEBHOOK_SECRET:
@@ -18,9 +19,18 @@ async def receive_webhook(request: Request):
     data = await request.json()
     print("\n📩 Webhook Data Received:")
     print(json.dumps(data, indent=4))
+    ai_reply = generate_ticket_reply(data)
+    
+    with open("webhook_log.json", "a") as f:
+        f.write(json.dumps({
+            "received_at": str(datetime.now()),
+            "request": data,
+            "ai_reply": ai_reply,
+        }) + "\n")
 
     return {
         "status": "success",
         "message": "Webhook received!",
+        "ai_reply":ai_reply,
         "received_data": data
     }
